1ce6282dbb3d5691f76a3e9bead1839021629caf
Emmanuel <fl6461638@gmail.com>
2026-04-28 18:21:08 +0100

    Add Resend welcome email

--- Backend/email.js
+++ Backend/email.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("paste_your_api_key_here");
 
 export async function sendWelcomeEmail(to, name) {
   try {
