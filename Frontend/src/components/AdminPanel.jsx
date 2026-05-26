24766eb18b74aa5060b87d972929e10ef44788c5
chainpilot-dev360 <fl6461638@gmail.com>
2026-05-26 14:44:41 +0100

    Update AdminPanel.jsx

--- Frontend/src/components/AdminPanel.jsx
+++ Frontend/src/components/AdminPanel.jsx
@@ -753,18 +753,6 @@ async function updateKyc(id, status) {
               Reject Withdrawal
             </button>
           </div>
-        )}
-
-      {withdrawals.length > withdrawalLimit && (
-        <div style={{ marginTop: "15px" }}>
-          <button
-            onClick={() => setWithdrawalLimit((prev) => prev + 10)}
-            style={buttonSecondary}
-          >
-            Load More Withdrawals
-          </button>
-        </div>
-      )}
 
       <h3>Admin Activity Log</h3>
 
